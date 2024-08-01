38edb2fbf4b7666ea6460def1281398333f8db0e
VietfromPKA <22010256@st.phenikaa-uni.edu.vn>
2024-08-02 01:00:54 +0700

    update contact + form tư vấn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//xem them thông tin
+
 document.getElementById('readMoreBtn').addEventListener('click', function() {
     var contentAn = document.getElementById('contentAn');
     var overlay = document.getElementById('overlay');
@@ -18,3 +20,19 @@ document.getElementById('overlay').addEventListener('click', function() {
     contentAn.style.display = 'none';
     overlay.style.display = 'none';
 });
+
+// đăng ký tư vấn 
+
+document.getElementById('readBtn').addEventListener('click', function() {
+    document.getElementById('tvContent').style.display = 'flex';
+});
+
+document.getElementById('close_Btn').addEventListener('click', function() {
+    document.getElementById('tvContent').style.display = 'none';
+});
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('tvContent')) {
+        document.getElementById('tvContent').style.display = 'none';
+    }
+}
